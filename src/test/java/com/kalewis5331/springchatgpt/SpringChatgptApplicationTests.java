f26216e62c65febccf9cfca56b60b143b599092e
kalewis5331 <kalewis5331@gmail.com>
2024-03-22 13:21:59 -0400

    Initialized Project via Spring Web

---
+++ src/test/java/com/kalewis5331/springchatgpt/SpringChatgptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalewis5331.springchatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatgptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
